1684a995be9d7ecbcb47952edc5daca73979210b
hadyelsahar <hadyelsahar@gmail.com>
2017-09-06 14:52:20 +0200

    adapt the project for text

--- evaluator/eval.py
+++ evaluator/eval.py
@@ -1,34 +1,40 @@
-__author__ = 'tylin'
+__author__ = 'hadyelsahar'
 from tokenizer.ptbtokenizer import PTBTokenizer
 from bleu.bleu import Bleu
 from meteor.meteor import Meteor
 from rouge.rouge import Rouge
 from cider.cider import Cider
 
-class COCOEvalCap:
-    def __init__(self, coco, cocoRes):
-        self.evalImgs = []
-        self.eval = {}
-        self.imgToEval = {}
-        self.coco = coco
-        self.cocoRes = cocoRes
-        self.params = {'image_id': coco.getImgIds()}
+class Evaluator:
+    def __init__(self, predicted_list, label_list):
+        """
+
+        :param predicted_list: list of predicted strings
+        :param label_list: list of lists of true labels 1 item can have several true labels
+        """
+
+        self.eval = []
+        self.overall_eval = {}
+        self.id_to_eval = {}
+
+        self.predicted_list = [[i] for i in predicted_list]
+
+        if type(label_list[0]) is str:
+            label_list = [[i] for i in label_list]
+        self.label_list = label_list
 
     def evaluate(self):
-        imgIds = self.params['image_id']
+
         # imgIds = self.coco.getImgIds()
-        gts = {}
-        res = {}
-        for imgId in imgIds:
-            gts[imgId] = self.coco.imgToAnns[imgId]
-            res[imgId] = self.cocoRes.imgToAnns[imgId]
+        gts = dict(zip(range(0, len(self.predicted_list)), self.predicted_list))
+        res = dict(zip(range(0, len(self.label_list)), self.label_list))
 
         # =================================================
         # Set up scorers
         # =================================================
         print 'tokenization...'
         tokenizer = PTBTokenizer()
-        gts  = tokenizer.tokenize(gts)
+        gts = tokenizer.tokenize(gts)
         res = tokenizer.tokenize(res)
 
         # =================================================
@@ -51,23 +57,23 @@ class COCOEvalCap:
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
-                    self.setImgToEvalImgs(scs, gts.keys(), m)
+                    self.set_textid_to_eval(scs, gts.keys(), m)
                     print "%s: %0.3f"%(m, sc)
             else:
                 self.setEval(score, method)
-                self.setImgToEvalImgs(scores, gts.keys(), method)
+                self.set_textid_to_eval(scores, gts.keys(), method)
                 print "%s: %0.3f"%(method, score)
-        self.setEvalImgs()
+        self.set_eval()
 
     def setEval(self, score, method):
-        self.eval[method] = score
+        self.overall_eval[method] = score
 
-    def setImgToEvalImgs(self, scores, imgIds, method):
+    def set_textid_to_eval(self, scores, imgIds, method):
         for imgId, score in zip(imgIds, scores):
-            if not imgId in self.imgToEval:
-                self.imgToEval[imgId] = {}
-                self.imgToEval[imgId]["image_id"] = imgId
-            self.imgToEval[imgId][method] = score
+            if not imgId in self.id_to_eval:
+                self.id_to_eval[imgId] = {}
+                self.id_to_eval[imgId]["image_id"] = imgId
+            self.id_to_eval[imgId][method] = score
 
-    def setEvalImgs(self):
-        self.evalImgs = [eval for imgId, eval in self.imgToEval.items()]
+    def set_eval(self):
+        self.eval = [eval for imgId, eval in self.id_to_eval.items()]
